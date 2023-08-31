b8a27efd3d6cf8afa7670663ba538be9075283d8
khs200 <113793971+khs200@users.noreply.github.com>
2023-08-31 20:32:50 +0900

    Add files via upload

---
+++ meditect/Meditect-project1/Meditect-project/meditect/src/main/java/com/comjeongsaek/meditect/MeditectApplication.java
@@ -0,0 +1,13 @@
+package com.comjeongsaek.meditect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditectApplication.class, args);
+	}
+
+}
---
+++ meditect/Meditect-project1/Meditect-project/meditect/src/test/java/com/comjeongsaek/meditect/MeditectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comjeongsaek.meditect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
